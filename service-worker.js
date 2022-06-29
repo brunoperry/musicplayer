bd98598fdc7e339783c93a56e235bbf618543b81
Bruno Perry <brunoperry@Brunos-MBP.lan>
2022-06-29 23:37:12 +0100

    Initial dist subtree commit

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,22 @@
 const OFFLINE_VERSION = 1;
-const CACHE_NAME = "offline";
+const OFFLINE_CACHE_NAME = "offline";
 const OFFLINE_URL = "offline.html";
 const offlineAssets = ["offline.html"];
+
+const ONLINE_CACHE_NAME = "online";
 const ONLINE_URL = "index.html";
 const onlineAssets = ["index.html"];
 
 self.addEventListener("install", (installEvent) => {
+  console.log("install sw");
   installEvent.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(OFFLINE_CACHE_NAME).then((cache) => {
       cache.addAll(offlineAssets);
     })
   );
 });
 self.addEventListener("activate", (event) => {
+  console.log("activate sw");
   event.waitUntil(
     (async () => {
       if ("navigationPreload" in self.registration) {
@@ -23,8 +27,8 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 self.addEventListener("fetch", (event) => {
-  console.log("fetching sw");
   if (event.request.mode === "navigate") {
+    console.log("navigate sw");
     event.respondWith(
       (async () => {
         try {
@@ -38,7 +42,7 @@ self.addEventListener("fetch", (event) => {
         } catch (error) {
           console.log("Fetch failed; returning offline page instead.", error);
 
-          const cache = await caches.open(CACHE_NAME);
+          const cache = await caches.open(OFFLINE_CACHE_NAME);
           const cachedResponse = await cache.match(OFFLINE_URL);
           return cachedResponse;
         }
